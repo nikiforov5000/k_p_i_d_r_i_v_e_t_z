d3f800b722ecb93784b1722526ab1fe807cb946f
boris <nikiforov5000@gmail.com>
2023-08-31 09:32:28 +0300

    requests parsed from json into Indicator class

--- lib/main.dart
+++ lib/main.dart
@@ -61,12 +61,64 @@ class Requests {
 
     if (response.statusCode == 200) {
       print('code 200');
-      var jsonResponse =
-          convert.utf8.decode(response.bodyBytes);
-      // print('response is:' + jsonResponse.toString());
-      // print('response is:' + jsonResponse.toString());
+      final responseBody = convert.utf8.decode(response.bodyBytes);
+      final jsonResponse = convert.jsonDecode(responseBody) as Map<String, dynamic>;
+      final data = jsonResponse['DATA'];
+      final rows = data['rows'] as List;
+
+      final indicators = rows.map((json) => Indicator.fromJson(json)).toList();
+
+
+      print('response is:' + indicators.toString());
     } else {
       print('code ${response.statusCode.toString()}');
     }
   }
 }
+
+class Indicator {
+  final String name;
+  final int indicatorToMoId;
+  final int parentId;
+  final int order;
+
+  Indicator({
+    required this.name,
+    required this.indicatorToMoId,
+    required this.parentId,
+    required this.order,
+  });
+
+  factory Indicator.fromJson(Map<String, dynamic> json) {
+    return Indicator(
+      name: json['name'],
+      indicatorToMoId: json['indicator_to_mo_id'],
+      parentId: json['parent_id'],
+      order: json['order'],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
